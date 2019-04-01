1d9b587d40a40be9c2ec24693f358f429c0b255b
thealexvarga <thealexvarga@gmail.com>
2019-04-02 00:56:42 +0200

    file selector in bot editor that opens and parses .motp files

--- MotP 0/ui/hud/BotEditor/BotEditor.gd
+++ MotP 0/ui/hud/BotEditor/BotEditor.gd
@@ -26,6 +26,23 @@ func _on_OptionButton_item_selected(m):
 	edited.set_mode(m)
 	hide()
 
+func _on_FileButton_pressed():
+	$FileDialog.show()
+
+func _on_FileDialog_file_selected(filename):
+	var f = File.new()
+	f.open(filename, f.READ)
+	var text = f.get_as_text()
+	f.close()
+	var obj = JSON.parse(text).result
+	print(obj["mode"])
+
+	# var p = JSON.parse('["hello", "world", "!"]')
+	# if typeof(p.result) == TYPE_ARRAY:
+	# 	print(p.result[0]) # prints 'hello'
+	# else:
+	# 	print("unexpected results")
+
 func exit():
 	if editor:
 		editor.paused = false
